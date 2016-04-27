0b64f2d1a589c1d330632a1d641bc3930ab821ab
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2016-04-27 09:08:20 +0200

    testing the chat function

--- index.js
+++ index.js
@@ -14,8 +14,15 @@ http.listen(port, function(){
 io.on('connection', function(socket){
   console.log('a user connected');
 
+  socket.on('chat message', function(msg){
+    console.log(`got message '${msg}', broadcasting to all`);
+    io.emit('chat message', msg);
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
 
+// export app so we can test it
+exports = module.exports = app;
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var should = require('should');
 var io = require('socket.io-client');
+var app = require('./index');
 
 var socketURL = 'http://0.0.0.0:5000';
 
@@ -8,11 +9,38 @@ var options ={
       'force new connection': true
 };
 
-var user1 = {'name':'Dana'};
-var user2 = {'name':'Sally'};
-var user3 = {'name':'Tom'};
-
 describe("Auction Server",function(){
+  it('Should echo chat massages back to user', function(done){
+    var client1 = io.connect(socketURL, options);
+
+    client1.on('connect', function(data){
+      client1.emit('chat message', 'hello world');
+      client1.on('chat message', function(data){
+        console.log('got back ' + data);
+        data.should.equal('hello world');
+        client1.disconnect();
+        done();
+      });
+    });
+  });
+  it('Should broadcast chat massages to all users', function(done){
+    var client1 = io.connect(socketURL, options);
+
+    client1.on('connect', function(data){
+      var client2 = io.connect(socketURL, options);
+
+      client2.on('connect', function(data){
+        client2.emit('chat message', 'hello world');
+      });
+
+      client1.on('chat message', function(data){
+        data.should.equal('hello world');
+        client1.disconnect();
+        client2.disconnect();
+        done();
+      });
+    });
+  });
 
 });
 
